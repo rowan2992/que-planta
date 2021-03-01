4b884c3adcf2d68fe26945a3e30185a7ce4726f7
Rowan Douglas <rowan.douglas29@gmail.com>
2021-03-01 14:56:56 -0300

    react setup

---
+++ app/javascript/que_planta/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger'
+import ReduxPromise from 'redux-promise';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import App from './components/app';
+// import messagesReducer from './reducers/messages_reducer';
+
+const Planta = props => (
+  <div>Hello {props.name}!</div>
+)
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Planta name="Plants plants plants!" />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
+
+const initialState = {
+  // messages: [],
+  // channels: JSON.parse(chatContainer.dataset.channels).map(c => c.name)
+};
+
+const reducers = combineReducers({
+  // messages: messagesReducer,
+  // channels: (state = null, action) => state
+});
+
+const middlewares = applyMiddleware(logger, ReduxPromise);
+const store = createStore(reducers, initialState, middlewares);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+);
