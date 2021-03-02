394f65113d4b023e6cdccfc1ff26d57737730054
Rowan Douglas <rowan.douglas29@gmail.com>
2021-03-02 15:13:37 -0300

    react index component implemented

--- app/javascript/packs/planta.js
+++
@@ -1,1 +0,0 @@
-import '../que_planta';
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # get 'homepage/index'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# Rails.application.routes.draw do
+#   root 'homepage#index'
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
