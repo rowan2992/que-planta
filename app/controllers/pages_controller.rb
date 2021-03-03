82e46f89989bfe3c6fb46092e5745816378aef84
Rowan Douglas <rowan.douglas29@gmail.com>
2021-03-03 13:57:59 -0300

    started scraping testing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
+require 'httparty'
+require 'json'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    r = HTTParty.get(
+      'https://trefle.io/api/v1/species/search',
+      query: {
+        "q": "stickybob",
+        "token": "X0QJXUfcbDt6ftn-IkC4YkkpIDIvt_59l9l6KbEyDnQ"
+      }
+    )
+    # token = 'X0QJXUfcbDt6ftn-IkC4YkkpIDIvt_59l9l6KbEyDnQ'
+    # query = 'coconut'
+    # JSON.parse("https://trefle.io/api/v1/plants/search?token=#{token}&q=#{query}")
+
+    @plants = r.parsed_response['data']
+
   end
 end
